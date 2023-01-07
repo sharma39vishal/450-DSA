c8da83bab44979ecc8944a2b6e96110f576aa52f
sharma39vishal <sharma39vishal@gmail.com>
2023-01-07 11:05:23 +0530

    2 string question

---
+++ String/344. Reverse String.cpp
@@ -0,0 +1,7 @@
+void reverseString(vector<char>& s) {
+        int j=s.size()-1;
+        for(int i=0;i<s.size()/2;i++){
+            swap(s[i],s[j]);
+            j--;
+        }
+    }
---
+++ String/Palindrome String.cpp
@@ -0,0 +1,13 @@
+	
+	int isPalindrome(string s)
+	{
+	    int i=0,j=s.length()-1;
+	    while(i<j){
+	        if(s[i]!=s[j]){
+	            return 0;
+	        }
+	        i++;
+	        j--;
+	    }
+	    return 1;
+	}
